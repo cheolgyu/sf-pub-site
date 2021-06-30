723f965503517931da366d172430d54013b2bad6
cheolgyu <rb7480@knou.ac.kr>
2021-06-30 11:46:57 +0900

    view gamebot

---
+++ src/components/gamebot/PlayList.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="play_area" v-if="loaded">
+    <h6>바람의나라 연</h6>
+    <iframe
+      width="560"
+      height="315"
+      src="https://www.youtube-nocookie.com/embed/7n4ax93YGdg"
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+    ></iframe>
+    <br />
+    <hr />
+    <br />
+    <h6>왕자의게임</h6>
+    <iframe
+      v-if="loaded"
+      width="560"
+      height="315"
+      src="https://www.youtube-nocookie.com/embed/zg9mrhnAg3o"
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+    ></iframe>
+    <br />
+    <hr />
+    <br />
+    <h6>라오킹</h6>
+    <iframe
+      v-if="loaded"
+      width="560"
+      height="315"
+      src="https://www.youtube-nocookie.com/embed/iBO9y7JlAYw"
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+    ></iframe>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      loaded: false,
+    };
+  },
+  mounted() {
+    this.loaded = true;
+  },
+};
+</script>
+<style lang="scss">
+.play_area {
+  width: 800px;
+  margin: auto;
+}
+</style>
--- src/views/GameBot.vue
+++ src/views/GameBot.vue
@@ -1,3 +1,54 @@
 <template>
-  <h1>GameBot</h1>
+  <div>
+    <h3>
+      현재 게임 화면을 캡쳐하여, 원하는 이미지를 찾은후 이벤트 발생시키기.
+    </h3>
+    <p>yolov4, tensorflow, android</p>
+    <br />
+    <div class="dl_area">
+      <dl>
+        <dt>게임봇</dt>
+        <dd>현재 화면 캡쳐</dd>
+        <dd>클릭 이벤트 or 카카오알림 실행</dd>
+        <dt>보글보글</dt>
+        <dd>현재 입력되는 소리 캡쳐</dd>
+        <dd>진동 or 플래시 or 소리 실행</dd>
+      </dl>
+    </div>
+    <p style="color: white">
+      <a
+        target="_blank"
+        href="https://github.com/cheolgyu?tab=repositories&q=gamebot"
+        >github</a
+      >
+      |
+      <a target="_blank" href="https://apkplz.net/developer/highserpot"
+        >apk( apkplz )</a
+      >
+    </p>
+    <br />
+    <PlayList />
+  </div>
+
 </template>
+<script>
+import PlayList from "@/components/gamebot/PlayList";
+export default {
+  components: {
+    PlayList,
+  },
+};
+</script>
+<style lang="scss" scoped>
+a {
+  color: wheat;
+}
+.dl_area {
+  width: 30%;
+  text-align: center;
+  margin: auto;
+}
+dl {
+  text-align: left;
+}
+</style>
